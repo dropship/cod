12d30fcb8a719a6d33e0a27bf90cbc23f9e3555f
Alex Southgate <southgate@gmail.com>
2014-08-18 10:31:29 -0700

    Change to match 512b cc3000 RX buffer

--- src/cod.ino
+++ src/cod.ino
@@ -26,7 +26,7 @@
 // These can be any two pins
 #define ADAFRUIT_CC3000_VBAT  5
 #define ADAFRUIT_CC3000_CS    10
-#define CC3000_BUFFER_SIZE    256
+#define CC3000_BUFFER_SIZE    512
 
 // Use hardware SPI for the remaining pins
 // On an UNO, SCK = 13, MISO = 12, and MOSI = 11
